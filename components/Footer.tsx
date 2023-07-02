c7089171facaf5fd8ef3bd2e79774f21e62b519f
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2023-07-02 22:00:19 +0200

    First commit

---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {};
+
+const Footer = (props: Props) => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; povered by Shopeeno.com, All rights reserved.{" "}
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
