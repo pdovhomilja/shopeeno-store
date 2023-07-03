baf36463a8b2bf04b4d87773302158dc019c8a5d
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2023-07-04 00:38:07 +0200

    hotfix

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -36,7 +36,7 @@ const CategoryPage = async ({ params, searchParams }: CategoryPageProps) => {
   return (
     <div className="bg-white">
       <Container>
-        <Billboard data={category?.billboard} />
+        <Billboard data={category.billboard} />
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
             {/* Add mobile Filters */}
